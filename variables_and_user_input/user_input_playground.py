ac2448957e4e8b1bc69d0763de9771d3a1e68645
JVDBNE <jessvandyk@hotmail.com>
2022-04-30 09:25:53 +1000

    First Lesson
    initial push

---
+++ variables_and_user_input/user_input_playground.py
@@ -0,0 +1,7 @@
+name= input ( "What is Your Name?")
+print ("hello " + name + ", It is great to meet you!")
+
+
+number = int(input("Enter a number"))
+print (int(number)+ 5)
+print(number +5)
---
+++ variables_and_user_input/variables_playground.py
@@ -0,0 +1,41 @@
+# height = 168
+# weight = 108.5 
+
+#print (type(height))
+#print (type(weight))
+
+
+# day = "Saturday"
+# #print (type(day))
+# message = f"Today is {day}"
+# message2 = "Today is " + day + "!"
+# print (message)
+# print (message2)
+
+
+#Intergers and addition
+#Run distance in mi
+name = ("Jessica")
+
+run1_dist = 1400
+run2_dist = 1800
+
+# total_dist = run1_dist + run2_dist
+# print (total_dist)
+
+
+# #floats
+run_dist3 = 1.7
+run_dist4 = 1.35
+
+# total_dist2 = run_dist3 + run_dist4
+# print (total_dist2)
+
+
+#Division and multiplication
+
+print (run1_dist / 1000) #Division (will always produce float data type
+print (run_dist3 * 1000) #multiplication
+
+#typecast 
+print (name * str(run1_dist))
